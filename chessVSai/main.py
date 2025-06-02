d5022332a86ba52704bdbdaec9fb891b96b72ca1
GuiguiSlide <guillaum29280.fr@gmail.com>
2025-06-02 13:30:40 +0200

    organisation des fichiers terminée

--- chessVSai/main.py
+++ chessVSai/main.py
@@ -1,1 +1,8 @@
 from ursina import *
+def main():
+    app = Ursina()
+    cube = Entity(model='cube', color=color.azure, scale=(2,2,2), position=(0,0,0))
+    app.run()
+
+if __name__ == "__main__":
+    main()
