eef5bca2484c40fa1aa01afdba7ab9a8e7280c87
GuiguiSlide <guillaum29280.fr@gmail.com>
2025-06-02 13:31:04 +0200

    commencement d'un nouveu projet

--- chessVSai/main.py
+++ chessVSai/main.py
@@ -1,7 +1,7 @@
 from ursina import *
 def main():
     app = Ursina()
-    cube = Entity(model='cube', color=color.azure, scale=(2,2,2), position=(0,0,0))
+    cube = Entity(model='cube', color=color.white, scale=(2,2,2), position=(0,0,0))
     app.run()
 
 if __name__ == "__main__":
